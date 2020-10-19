16387a5e334e60fe977953a5fc01f242e36bc03e
Tomasz Omeljaniuk <tomekomel@gmail.com>
2020-10-19 23:50:31 +0200

    feat: add controller stub

--- src/decision/decision.module.ts
+++ src/decision/decision.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DecisionService } from './services/decision.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { decisionSchema } from './schemas/decision.schema';
+import { DecisionController } from './controllers/decision.controller';
 
 @Module({
-  providers: [DecisionService]
+  imports: [
+    MongooseModule.forFeature([{ name: 'Decision', schema: decisionSchema }]),
+  ],
+  controllers: [DecisionController],
+  providers: [DecisionService],
 })
 export class DecisionModule {}
