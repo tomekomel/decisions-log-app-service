a8b86611f32e47642adf6d0f30347a7fa6691e0b
Tomasz Omeljaniuk <tomekomel@gmail.com>
2020-10-20 23:41:19 +0200

    feat: add update and delete to controller

--- src/decision/controllers/decision.controller.ts
+++ src/decision/controllers/decision.controller.ts
@@ -1,7 +1,18 @@
-import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
+
 import { DecisionService } from '../services/decision.service';
 import { CreateDecisionDto } from '../dtos/create-decision.dto';
-import { Response } from 'express';
 
 @Controller('decisions')
 export class DecisionController {
@@ -16,5 +27,45 @@ export class DecisionController {
   async getAllDecisions(@Res() res: Response) {
     const decisions = await this.decisionService.getAllDecisions();
     return res.status(HttpStatus.OK).json(decisions);
+  }
+
+  @Get('/:decisionId')
+  async getDecisionById(
+    @Res() res: Response,
+    @Param('decisionId') decisionId: string,
+  ) {
+    const decision = await this.decisionService.getDecision(decisionId);
+    if (!decision) {
+      throw new NotFoundException('Decision not found');
+    }
+    return res.status(HttpStatus.OK).json(decision);
+  }
+
+  @Post('/:decisionId/update')
+  async updateDecision(
+    @Res() res: Response,
+    @Param('decisionId') decisionId: string,
+    @Body() createDecisionDto: CreateDecisionDto,
+  ) {
+    const decision = await this.decisionService.updateDecision(
+      decisionId,
+      createDecisionDto,
+    );
+    if (!decision) {
+      throw new NotFoundException('Decision not found');
+    }
+    return res.status(HttpStatus.OK).json(decision);
+  }
+
+  @Delete('/:decisionId')
+  async deleteDecision(
+    @Res() res: Response,
+    @Param('decisionId') decisionId: string,
+  ) {
+    const decision = await this.decisionService.deleteDecision(decisionId);
+    if (!decision) {
+      throw new NotFoundException('Decision not found');
+    }
+    return res.status(HttpStatus.NO_CONTENT);
   }
 }
