c8c8a66d0c4a7e7b1c96f34d3543cc376f38ee13
Tomasz Omeljaniuk <tomekomel@gmail.com>
2020-11-03 09:44:03 +0100

    feat: remove HRP, use start:dev for hot reload

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3000);
-
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
 }
 bootstrap();
