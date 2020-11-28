b8bdd2bb2c7fa39ba1f01e2886d8938a983466d4
Tomasz Omeljaniuk <tomekomel@gmail.com>
2020-11-28 16:43:30 +0100

    feat: remove healthchecks

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import * as Express from 'express';
-
-declare const module: any;
-
-const server = Express();
-server.get('/', (req, res) => res.send('ok'));
-server.get('/_ah/health', (req, res) => res.send('ok'));
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
